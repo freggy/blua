baa21da87e1edb64500beebf820cd24e2c9bbe1b
yannic rieger <yannic.rieger@hetzner-cloud.de>
2024-04-05 03:15:51 +0200

    improve code docs

--- javadump/src/main/kotlin/dev/freggy/ll/javadump/Data.kt
+++ javadump/src/main/kotlin/dev/freggy/ll/javadump/Data.kt
@@ -2,11 +2,27 @@ package dev.freggy.ll.javadump
 
 import kotlinx.serialization.Serializable
 
+/**
+ * @param name name of the parameter
+ * @param type parameter type
+ * @param desc comment part of its javadoc, not containing block tags
+ */
 @Serializable
 data class Param(val name: String, val type: String, val desc: String)
 
+/**
+ * @param name name of the method
+ * @param params parameter belonging to this class
+ * @param desc comment part of its javadoc, not containing block tags
+ * @param ret return type of this method
+ */
 @Serializable
 data class Method(val name: String, val params: List<Param>, val desc: String, val ret: String)
 
+/**
+ * @param FQCN fully qualified class name e.g. org.bukkit.Bukkit
+ * @param methods mehtods belonging to this class
+ * @param desc comment part of its javadoc, not containing block tags
+ */
 @Serializable
 data class Class(val FQCN: String, val methods: List<Method>, val desc: String)
--- javadump/src/main/kotlin/dev/freggy/ll/javadump/Main.kt
+++ javadump/src/main/kotlin/dev/freggy/ll/javadump/Main.kt
@@ -6,6 +6,7 @@ import com.github.javaparser.utils.SourceRoot
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
+import java.util.Optional
 import kotlin.io.path.Path
 import kotlin.jvm.optionals.getOrNull
 import kotlin.system.exitProcess
@@ -15,42 +16,43 @@ fun main(args: Array<String>) {
     val inputDir = argsMap["--in"]
     val outFile = argsMap["--out"]
 
-    checkArg(inputDir)
-    checkArg(outFile)
+    dieWhenNull(inputDir)
+    dieWhenNull(outFile)
 
     val classes = mutableListOf<Class>()
     val root = SourceRoot(Path(inputDir!!))
 
+    // "" searches through all available packages
+    // within the directory passed by --in
     root.parse("") { _, _, result ->
         result.result.ifPresent { cu ->
             cu.types.forEach { t ->
                 val methods = mutableListOf<Method>()
                 for (m in t.methods) {
-                    if (m.javadoc.isEmpty) {
-                        continue
-                    }
-                    val doc = m.javadoc.get()
+                    val doc = m.javadoc
                     val params = m.parameters.map { p ->
                         Param(
                             p.nameAsString,
                             p.typeAsString,
-                            getParamDoc(doc, p.nameAsString)
+                            javadocOrEmpty(doc) { getParamText(it, p.nameAsString) }
                         )
                     }.toList()
                     methods.add(
                         Method(
                             m.nameAsString,
                             params,
-                            doc.description.toText(),
-                            getReturnDoc(doc)
+                            javadocOrEmpty(doc) { it.description.toText() },
+                            javadocOrEmpty(doc) { getReturnText(it) }
                         )
                     )
                 }
-                val classDesc = when (t.javadoc.isPresent) {
-                    true -> t.javadoc.get().description.toText()
-                    else -> ""
-                }
-                classes.add(Class(t.fullyQualifiedName.get(), methods, classDesc))
+                classes.add(
+                    Class(
+                        t.fullyQualifiedName.get(),
+                        methods,
+                        javadocOrEmpty(t.javadoc) { t.javadoc.get().description.toText() }
+                    )
+                )
                 println(t.fullyQualifiedName.getOrNull())
             }
         }
@@ -63,7 +65,20 @@ fun main(args: Array<String>) {
     File(outFile!!).writeText(Json.encodeToString(classes))
 }
 
-fun getParamDoc(doc: Javadoc, param: String): String {
+fun javadocOrEmpty(opt: Optional<Javadoc>, f: (Javadoc) -> (String)):  {
+    return when (opt.isPresent) {
+        true -> f(opt.get())
+        else -> ""
+    }
+}
+
+/**
+ * gets text of a @param tag.
+ *
+ * @param doc [Javadoc] object the tag belongs to
+ * @param param param name
+ */
+fun getParamText(doc: Javadoc, param: String): String {
     val tag = doc.blockTags
         .filter { tag -> tag.type == JavadocBlockTag.Type.PARAM }
         .firstOrNull { tag -> tag.name.getOrNull() == param }
@@ -71,13 +86,18 @@ fun getParamDoc(doc: Javadoc, param: String): String {
     return tag.content.toText()
 }
 
-fun getReturnDoc(doc: Javadoc): String {
+/**
+ * gets text of a @return tag.
+ *
+ * @param doc [Javadoc] object the tag belongs to
+ */
+fun getReturnText(doc: Javadoc): String {
     val tag = doc.blockTags.firstOrNull { tag -> tag.type == JavadocBlockTag.Type.RETURN }
     tag ?: return ""
     return tag.content.toText()
 }
 
-fun checkArg(arg: String?) {
+fun dieWhenNull(arg: String?) {
     if (arg == null) {
         println("usage: javadump -in <path/to/java/src> -out <path/to/output>")
         exitProcess(1)
