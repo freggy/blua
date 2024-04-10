56c52f0cc55db7d3ef5277e77ec8d6bf1bbc97a6
Yannic Rieger <ybr@76k.io>
2024-04-10 20:55:46 +0200

    implement resolving fqcn of params and return types

--- javadump/src/main/kotlin/dev/freggy/ll/javadump/Data.kt
+++ javadump/src/main/kotlin/dev/freggy/ll/javadump/Data.kt
@@ -8,16 +8,27 @@ import kotlinx.serialization.Serializable
  * @param desc comment part of its javadoc, not containing block tags
  */
 @Serializable
-data class Param(val name: String, val type: String, val desc: String)
+data class Param(
+    val name: String,
+    val type: String,
+    val desc: String,
+)
 
 /**
  * @param name name of the method
  * @param params parameter belonging to this class
  * @param desc comment part of its javadoc, not containing block tags
- * @param ret return type of this method
+ * @param retTypeFQCN fully qualified class name of methods return type
+ * @param retDesc @return tag comment text
  */
 @Serializable
-data class Method(val name: String, val params: List<Param>, val desc: String, val ret: String)
+data class Method(
+    val name: String,
+    val params: List<Param>,
+    val desc: String,
+    val retTypeFQCN: String,
+    val retDesc: String,
+)
 
 /**
  * @param FQCN fully qualified class name e.g. org.bukkit.Bukkit
@@ -25,4 +36,8 @@ data class Method(val name: String, val params: List<Param>, val desc: String, v
  * @param desc comment part of its javadoc, not containing block tags
  */
 @Serializable
-data class Class(val FQCN: String, val methods: List<Method>, val desc: String)
+data class Class(
+    val FQCN: String,
+    val methods: List<Method>,
+    val desc: String,
+)
