58a3fd26a3f073bb6b1b3c13a3540710df73c8df
Yannic Rieger <ybr@76k.io>
2024-04-13 21:38:50 +0200

    rename package ll to blua
    also adjust readme

--- javadump/src/main/kotlin/dev/freggy/blua/javadump/Data.kt
+++ javadump/src/main/kotlin/dev/freggy/blua/javadump/Data.kt
@@ -1,4 +1,4 @@
-package dev.freggy.ll.javadump
+package dev.freggy.blua.javadump
 
 import kotlinx.serialization.Serializable
 
--- javadump/src/main/kotlin/dev/freggy/blua/javadump/Main.kt
+++ javadump/src/main/kotlin/dev/freggy/blua/javadump/Main.kt
@@ -1,4 +1,4 @@
-package dev.freggy.ll.javadump
+package dev.freggy.blua.javadump
 
 import com.github.javaparser.ast.CompilationUnit
 import com.github.javaparser.ast.type.Type
