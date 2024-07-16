06f9500018a01536f3699e824d2211a1003a075f
yannic rieger <yannic.rieger@hetzner-cloud.de>
2024-07-16 20:13:59 +0200

    update minecraft to 1.21

--- plugin/src/main/kotlin/dev/freggy/blua/Main.kt
+++ plugin/src/main/kotlin/dev/freggy/blua/Main.kt
@@ -1,18 +1,21 @@
 package dev.freggy.blua
 
+import dev.freggy.blua.runtime.EventCallbacks
 import org.bukkit.Bukkit
+import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
-import party.iroiro.luajava.Lua
+import party.iroiro.luajava.LuaException
 
-class Main : JavaPlugin() {
+class Main : JavaPlugin(), Listener {
     private val scripts = mutableListOf<Script>()
+    private val eventCallbacks = EventCallbacks(this, this)
 
     override fun onEnable() {
         val logger = Bukkit.getLogger()
         this.dataFolder.mkdir()
         val found = this.dataFolder
             .listFiles { _, name -> name.endsWith(".lua") }
-            ?.map { Script(it.name, it.readText()) }
+            ?.map { Script(it.name, it.readText(), this.eventCallbacks) }
         if (found == null) {
            logger.warning("no lua files found in ${this.dataFolder.absolutePath}")
             return
@@ -20,7 +23,10 @@ class Main : JavaPlugin() {
         this.scripts.addAll(found)
         scripts.forEach {
             logger.info("running ${it.name}")
-            if (it.run() != Lua.LuaError.OK) {
+            try {
+                it.run()
+            } catch (e: LuaException) {
+                e.printStackTrace()
                 logger.warning("error occurred in ${it.name}: ${it.error()}")
             }
         }
