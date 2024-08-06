fc6b5b553f6deaa501d08c82d6ec4b02eb5479bc
yannic rieger <ybr@76k.io>
2024-08-06 21:53:03 +0200

    fix typo in metadata example

--- examples/metadata.lua
+++ examples/metadata.lua
@@ -3,11 +3,11 @@
 -- listen to our event, this returns a descriptor, which is needed when trying to unregister the listener
 listen("org.bukkit.event.player.PlayerJoinEvent", function(event)
     local player = event:getPlayer()
-    setmeta(p, "joinTime", os.date())
+    setmeta(player, "joinTime", os.date())
 end)
 
 listen("org.bukkit.event.player.PlayerQuitEvent", function(event)
     local player = event:getPlayer()
-    local time = getmeta(p, "joinTime")
+    local time = getmeta(player, "joinTime")
     print(player:getName() .. " joined at " .. time .. " before leaving")
 end)
