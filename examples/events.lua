e5761d09821adba30131d789dfc09ed609a73d71
yannic rieger <ybr@76k.io>
2024-08-16 11:22:00 +0200

    fix wrong variable name in event listener example

--- examples/events.lua
+++ examples/events.lua
@@ -1,7 +1,7 @@
 local joined = false
 
 -- listen to our event, this returns a descriptor, which is needed when trying to unregister the listener
-local joinListenerDescriptor = listen("org.bukkit.event.player.PlayerJoinEvent", function(event)
+local joinListener = listen("org.bukkit.event.player.PlayerJoinEvent", function(event)
     joined = true
 end)
 
